7156e754f2535d4398403c4b0ccee8aa7a9dca73
karen9999 <karen.dev999@gmail.com>
2021-04-28 14:37:13 +0300

    refactored sortTicket.js

--- src/utils/sortTickets.js
+++ src/utils/sortTickets.js
@@ -1,4 +1,4 @@
 export default {
-  'cheap': (tickets) => tickets.sort((a, b) => a.price - b.price),
-  'fast': (tickets) => tickets.sort((a, b) => b.price - a.price),
+  cheap: (tickets) => tickets.sort((ticketA, ticketB) => ticketA.price - ticketB.price),
+  fast: (tickets) => tickets.sort((ticketA, ticketB) => ticketB.price - ticketA.price),
 };
